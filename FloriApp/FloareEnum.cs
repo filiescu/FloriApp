3d1f3bfd19db530b657ca4e04a3449e2b8453982
Florina Iliescu <filiescu89@gmail.com>
2021-05-26 18:10:44 +0300

    Inlocuire if cu switch Creare enum pentru flori

---
+++ FloriApp/FloareEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace FloriApp
+{
+    public partial class FloriFactory
+    {
+        public enum FloareEnum
+        {
+            Trandafiri,
+            Lalele,
+            Muscate,
+            FloareDeHartie
+        }
+    }
+}
